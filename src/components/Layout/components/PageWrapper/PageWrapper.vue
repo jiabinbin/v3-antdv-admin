e24faea0bf215ddf535ef7c43ca09794b22b34b6
Jobin <jobin.jia@beefinx.com>
2020-09-03 20:00:21 +0800

    fix:bugs

--- src/components/Layout/components/PageWrapper/PageWrapper.vue
+++ src/components/Layout/components/PageWrapper/PageWrapper.vue
@@ -3,6 +3,7 @@ import { defineComponent, computed, reactive } from 'vue'
 import CusPageHeader from '@/components/Layout/components/PageHeader/PageHeader'
 import { useStore } from 'vuex'
 import PageHeader from 'ant-design-vue/es/page-header/index'
+import styles from './index.module.less'
 
 export default defineComponent({
   name: 'PageWrapper',
@@ -16,13 +17,16 @@ export default defineComponent({
     const menuList = computed(() => store.state.app.menuList)
     return () => (
       <CusPageHeader
+        class={styles['page-wrapper']}
         {...propsState}
         menuList={menuList.value}
         v-slots={
           slots
         }
       >
-        <slot></slot>
+        <div class={styles['page-wrapper-content']}>
+          <slot></slot>
+        </div>
       </CusPageHeader>
     )
   }
--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -58,5 +58,5 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
 </style>
